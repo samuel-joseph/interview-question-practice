e021d500ce7690f261c754cc6655784e3875f961
arcanlte <pinangayjoel@gmail.com>
2020-05-10 18:21:17 +0800

    Problem solved

--- tutorial.js
+++ tutorial.js
@@ -136,9 +136,7 @@ var isValid = function (s) {
     return false;
   } else {
     while (brackets.length !== 0) {
-      console.log(brackets[brackets.length - 1]);
       if (brackets[0] === "(" || brackets[0] === "{" || brackets[0] === "[") {
-        // if (brackets[1] === ")" || brackets[1] === "}" || brackets[1] === "]") {
         switch (brackets[0]) {
           case "(":
             if (brackets[1] === ")") {
@@ -171,20 +169,9 @@ var isValid = function (s) {
             return false;
         }
       }
-      // else if (
-      //     brackets[brackets.length - 1] === ")" ||
-      //     brackets[brackets.length - 1] === "}" ||
-      //     brackets[brackets.length - 1] === "]"
-      //   ) {
-      //     // brackets.splice(brackets[0], 1);
-      //     // brackets.pop();
-      //   }
-      // } else {
-      //   return false;
-      // }
     }
     return true;
   }
 };
 
-console.log(isValid("({[]})"));
+console.log(isValid(""));
