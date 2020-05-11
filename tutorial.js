e566965c1c64160fd72bd63971f78ace69e79e12
arcanlte <pinangayjoel@gmail.com>
2020-05-11 21:53:36 +0800

    Example 1 and 2 Solved

--- tutorial.js
+++ tutorial.js
@@ -166,7 +166,6 @@ var isValid = function (s) {
   else return false;
 };
 
-
 // Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
@@ -181,8 +180,14 @@ var isValid = function (s) {
 
 // It doesn't matter what you leave beyond the returned length.
 
-var removeElement = function(nums, val) {
-    
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
 };
 
-console.log(removeElement([3,2,2,3],3))
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
