ed9fb600e065aa643352dc1375abe9c5f2618b32
arcanlte <pinangayjoel@gmail.com>
2020-05-22 21:05:43 +0800

    ChecK

--- tutorial.js
+++ tutorial.js
@@ -331,19 +331,23 @@ var findTheDifference = function (s, t) {
 
 // console.log(findTheDifference("", "y"));
 
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
 
-// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+// If the last word does not exist, return 0.
 
-// Note: For the purpose of this problem, we define empty string as valid palindrome.
+// Note: A word is defined as a maximal substring consisting of non-space characters only.
 
-// Example 1:
+// Example:
 
-// Input: "A man, a plan, a canal: Panama"
-// Output: true
-// Example 2:
+// Input: "Hello World"
+// Output: 5
 
-// Input: "race a car"
-// Output: false
+var lengthOfLastWord = function (s) {
+  let sum = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") return sum;
+    sum = sum + 1;
+  }
+};
 
-
-
+console.log(lengthOfLastWord("Hello World"));
