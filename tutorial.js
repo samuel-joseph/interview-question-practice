25ac951626170270c01c2491f7f14421224a3a4e
arcanlte <pinangayjoel@gmail.com>
2020-05-22 18:23:12 +0800

    New problem.

--- tutorial.js
+++ tutorial.js
@@ -322,11 +322,28 @@ var findTheDifference = function (s, t) {
   var d = ("" + t).split("");
   for (let i = d.length - 1; i >= 0; i--) {
     let index = s.indexOf(d[i]);
+    if (d.length === 1) return d.pop();
     if (d[i] === s[index]) {
       d.splice(i, 1);
-      if (d.length === 1) return d.pop();
     }
   }
 };
 
-console.log(findTheDifference("abcd", "abcde"));
+// console.log(findTheDifference("", "y"));
+
+
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+// Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+// Example 1:
+
+// Input: "A man, a plan, a canal: Panama"
+// Output: true
+// Example 2:
+
+// Input: "race a car"
+// Output: false
+
+
+
