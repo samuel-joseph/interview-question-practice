d35c91d79008787d3170c310493be85d873b7a6a
arcanlte <pinangayjoel@gmail.com>
2020-05-27 21:01:17 +0800

    Testing

--- tutorial.js
+++ tutorial.js
@@ -376,48 +376,50 @@ var missingNumber = function (nums) {
 // console.log(missingNumber([1, 0]));
 
 var intToRoman = function (num) {
-  let answer = [];
+  let answer = "";
   while (num !== 0) {
     if (num >= 1000) {
-      answer.push("M");
+      answer += "M";
       num -= 1000;
     } else if (num >= 900) {
-      answer.push("CM");
+      answer += "CM";
       num -= 900;
     } else if (num >= 500) {
-      answer.push("D");
+      answer += "D";
       num -= 500;
     } else if (num >= 400) {
-      answer.push("CD");
+      answer += "CD";
       num -= 400;
     } else if (num >= 100) {
-      answer.push("C");
+      answer += "C";
       num -= 100;
     } else if (num >= 90) {
-      answer.push("XC");
+      answer += "XC";
       num -= 90;
     } else if (num >= 50) {
-      answer.push("L");
+      answer += "L";
       num -= 50;
     } else if (num >= 40) {
-      answer.push("XL");
+      answer += "XL";
       num -= 40;
     } else if (num >= 10) {
-      answer.push("X");
+      answer += "X";
       num -= 10;
     } else if (num >= 9) {
-      answer.push("IX");
+      answer += "IX";
       num -= 9;
     } else if (num >= 5) {
-      answer.push("V");
+      answer += "V";
       num -= 5;
     } else if (num >= 4) {
-      answer.push("IV");
+      answer += "IV";
       num -= 4;
     } else if (num >= 1) {
-      answer.push("I");
+      answer += "I";
       num -= 1;
     }
   }
-  return answer.toString();
+  return answer;
 };
+
+console.log(intToRoman(58));
