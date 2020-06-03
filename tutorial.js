87c79b384c23425900f4772de3af57b8d6e8967e
arcanlte <pinangayjoel@gmail.com>
2020-06-03 19:51:30 +0800

    New Problem Rearrange words in sentence

--- tutorial.js
+++ tutorial.js
@@ -537,4 +537,26 @@ var buildArray = function (target, n) {
   return answer;
 };
 
-console.log(buildArray([1, 2], 10));
+// Given a sentence text (A sentence is a string of space-separated words) in the following format:
+
+// First letter is in upper case.
+// Each word in text are separated by a single space.
+// Your task is to rearrange the words in text such that all words are rearranged in an increasing order of their lengths. If two words have the same length, arrange them in their original order.
+
+// Return the new text following the format shown above.
+
+ 
+
+// Example 1:
+
+// Input: text = "Leetcode is cool"
+// Output: "Is cool leetcode"
+// Explanation: There are 3 words, "Leetcode" of length 8, "is" of length 2 and "cool" of length 4.
+// Output is ordered by length and the new first word starts with capital letter.
+
+
+var arrangeWords = function(text) {
+    
+};
+
+console.log(arrangeWords("Leetcode is cool"))
