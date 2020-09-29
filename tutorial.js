4051251d5cd6267c05b5730aff66cd1099a46760
arcanlte <pinangayjoel@gmail.com>
2020-09-29 11:15:03 -0400

    New Problem

--- tutorial.js
+++ tutorial.js
@@ -745,4 +745,23 @@ var maxChar = function (str) {
   return maxChar;
 };
 
-console.log(maxChar("Hello There!"));
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+
+var fizzBuzz = function (n) {
+  
+}
+
+console.log(fizzBuzz(10))
