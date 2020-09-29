a1f61695148a871ee74cf0f419eda8ceb9a1bba6
arcanlte <pinangayjoel@gmail.com>
2020-09-29 12:48:49 -0400

    Array chunk problem

--- tutorial.js
+++ tutorial.js
@@ -759,9 +759,32 @@ var maxChar = function (str) {
 //   4
 //   buzz
 
-
 var fizzBuzz = function (n) {
-  
-}
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+};
 
-console.log(fizzBuzz(10))
+var chunk = function (arr, n) {
+  let temp = [];
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (temp === n) {
+      answer.push(temp);
+      temp = [];
+    } else {
+      temp.push[arr[i]];
+    }
+  }
+  return answer;
+};
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
