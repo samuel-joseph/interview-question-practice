93af16a6d70bbb76e2aef9b8c6390684f02e8ef1
arcanlte <pinangayjoel@gmail.com>
2020-09-30 21:01:34 -0400

    Capitalize problem

--- tutorial.js
+++ tutorial.js
@@ -818,4 +818,18 @@ var anagrams = function (stringA, stringB) {
   }
 };
 
-console.log(anagrams("abc!!!", "cbad"));
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+var capitalize = function (str) {
+  
+}
+
+console.log(capitalize('a short sentence'))
