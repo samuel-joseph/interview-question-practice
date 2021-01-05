eb0ec19fb7d7b10fa8ea68476d203a31443f95ae
arcanlte <pinangayjoel@gmail.com>
2021-01-05 13:32:22 -0500

    Test

--- goodPairs.js
+++ goodPairs.js
@@ -8,11 +8,14 @@ var numIdenticalPairs = function (nums) {
   let count = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[j] && i < j) {
-      count++;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j] && i < j) {
+        count++;
+      }
     }
   }
+
   return count;
 };
 
-
+console.log(numIdenticalPairs([1,2,3]));
