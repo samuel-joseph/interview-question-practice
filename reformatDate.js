ef9cdf106028f092cf233b567d15b8fd95585f88
arcanlte <pinangayjoel@gmail.com>
2021-01-07 13:57:55 -0500

    Object is resolved, first solution

--- reformatDate.js
+++ reformatDate.js
@@ -7,6 +7,29 @@
 // Input: date = "20th Oct 2052"
 // Output: "2052-10-20"
 
-var reformatDate = function(date) {
-    
+var reformatDate = function (date) {
+  var arr = date.split(" ");
+
+  var monthOptions = {
+    Jan: 1,
+    Feb: 2,
+    Mar: 3,
+    Apr: 4,
+    May: 5,
+    Jun: 6,
+    Jul: 7,
+    Aug: 8,
+    Sep: 9,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12,
+  };
+
+  var day = arr[0].substring(0, arr[0].length - 2);
+  var month = monthOptions[arr[1]];
+  var year = arr[2];
+
+  return year + "-" + month + "-" + day;
 };
+
+console.log(reformatDate("20th Oct 2052"));
