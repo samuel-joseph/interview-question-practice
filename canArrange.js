f57262096864cd4679ee3c215155acd937ae7e46
arcanlte <pinangayjoel@gmail.com>
2021-01-15 00:44:14 -0500

    Commit

--- canArrange.js
+++ canArrange.js
@@ -4,14 +4,25 @@
 
 // Return True If you can find a way to do that or False otherwise.
 
- 
-
 // Example 1:
 
 // Input: arr = [1,2,3,4,5,10,6,7,8,9], k = 5
 // Output: true
 // Explanation: Pairs are (1,9),(2,8),(3,7),(4,6) and (5,10).
 
-var canArrange = function(arr, k) {
-    
+var canArrange = function (arr, k) {
+  let isDivisible = arr.length / 2;
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; arr.length; j++) {
+      if (arr[i] + arr[j] === k) {
+        count++;
+      }
+    }
+  }
+  if (count >= isDivisible) {
+    return true;
+  }
+  return false;
 };
