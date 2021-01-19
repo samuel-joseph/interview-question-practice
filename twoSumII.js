b40e85f27cf65736a2f9341491689958ba41d4c7
arcanlte <pinangayjoel@gmail.com>
2021-01-19 10:05:33 -0500

    Problem is done

--- twoSumII.js
+++ twoSumII.js
@@ -13,10 +13,9 @@
 
 // Input: numbers = [2,7,11,15], target = 9
 // Output: [1,2]
-// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2
 
 var twoSum = function (numbers, target) {
-  // let answer = [];
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === target) {
