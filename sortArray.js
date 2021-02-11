ffe72f0f03826bd17c28a9b267f91cd881be6ec9
arcanlte <pinangayjoel@gmail.com>
2021-02-10 20:18:38 -0500

    Boilerplate initiated

---
+++ sortArray.js
@@ -0,0 +1,12 @@
+// Given an array of integers nums, sort the array in ascending order.
+
+// Example 1:
+
+// Input: nums = [5,2,3,1]
+// Output: [1, 2, 3, 5]
+
+var sortArray = function(nums) {
+    
+};
+
+console.log(sortArray([5,2,3,1]))
