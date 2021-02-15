67bbe14c311a185a46fae19b5890f2a24324fb4d
arcanlte <pinangayjoel@gmail.com>
2021-02-15 14:52:43 -0500

    First solution

---
+++ toLowerCase.js
@@ -0,0 +1,12 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+// Example 1:
+
+// Input: "Hello"
+// Output: "hello"
+
+var toLowerCase = function (str) {
+  return str.toLowerCase();
+};
+
+console.log(toLowerCase("Hello"));
