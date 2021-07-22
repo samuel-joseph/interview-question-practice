9cb7a6bde8742dac45f8e6b01c692001002f4891
arcanlte <pinangayjoel@gmail.com>
2021-07-22 17:19:27 -0400

    problem solved.

--- find_sum_of_two.js
+++ find_sum_of_two.js
@@ -1,6 +1,12 @@
 const find_sum_of_two = (a, val) => {
-
-  
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i + 1; j < a.length; j++) {
+      if (a[i] + a[j] === val) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
-console.log(find_sum_of_two(a, val));
+console.log(find_sum_of_two([5, 7, 1, 2, 8, 4, 3], 19));
